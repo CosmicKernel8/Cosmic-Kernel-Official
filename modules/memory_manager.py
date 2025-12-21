f776123f43f13a3bbe7144f70dd2f01274f1260e
CosmicKernel8 <lovehyena1@gmail.com>
2025-12-21 09:11:56 +0900

    Delete modules directory

--- modules/memory_manager.py
+++
@@ -1,7 +0,0 @@
-def run_blackhole_gc(self, cluster_density):
-    if cluster_density > self.expansion_limit:
-        # 허니팟 보안 로직 연동: 악성 고엔트로피 유도
-        print("[SECURITY] Redirecting high-entropy noise to Event Horizon.")
-        self.entropy_checksum += math.log2(cluster_density) 
-        return 0 # 싱귤래리티 초기화
-    return cluster_density
